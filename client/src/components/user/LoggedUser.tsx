17b59f41111e689e7dc02acb5ebaabc6817543f6
dimitarmitkov <dimitar.mitkov@me.com>
2022-03-22 15:51:04 +0200

    set logged user to function component

--- client/src/components/user/LoggedUser.tsx
+++ client/src/components/user/LoggedUser.tsx
@@ -1,40 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import axios from 'axios';
 import { Button } from 'primereact/button';
 
+const ShowUsersList = () => {
 
+    const [user, setUser] = useState(Object)
 
-
-export default class LoggedUser extends React.Component<any, any> {
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            users: []
-        };
-    }
-
-    componentDidMount() {
-        axios
-            .get('http://localhost:62000/api/v1/users/3')
-            .then(response => {
-                this.setState({
-                    users: response.data
-                })
-            })
+    function getEvents() {
+        axios.get("http://localhost:62000/api/v1/users/3")
+            .then(response => setUser(response.data))
             .catch(err => {
                 console.log('Error from ShowTasksList');
-            })
-    };
+            });
+    }
 
+    useEffect(() => {
+        getEvents()
+    }, []);
 
-    render() {
+    console.log(user);
+    
 
-        const loggedUser = this.state.users;
-
-        if (loggedUser.deletedAt) {
+        if (user.deletedAt) {
             return <Button icon="pi pi-user" className="p-button-rounded p-button-info" disabled />
         } else {
             return <Button icon="pi pi-user" className="p-button-rounded p-button-info" />
         }
-    }
 }
+
+export default ShowUsersList;
