2f50404359b402e0405e97eef8571a9d3328f2ef
dimitarmitkov <dimitar.mitkov@me.com>
2022-04-02 22:48:23 +0300

    apply use effect in modalTask

--- client/src/components/modal/ModalTask.tsx
+++ client/src/components/modal/ModalTask.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import { Button, DropdownButton, Dropdown, Form } from 'react-bootstrap';
 import axios from 'axios';
@@ -8,7 +8,7 @@ const MyVerticallyCenteredModal = (props: any) => {
 
     const [user, setUser] = useState(Object);
 
-  CurrentLoggedUser(setUser);
+    CurrentLoggedUser(setUser);
 
     interface MyObj {
         [propName: string]: {}
@@ -18,38 +18,52 @@ const MyVerticallyCenteredModal = (props: any) => {
     const meetingArray: string[] = ["initial", "selected", "progress", "done"];
     const currentDate = new Date(Date.now()).toISOString();
 
+    let queryData = {};
+
     const actionDataObject: MyObj = {
         initial: { initiatedAt: currentDate, initiatedByUserId: props.data.userId ? props.data.userId : user.id },
-        selected: { selectedAt: currentDate, selectedByUserId: props.data.userId ? props.data.userId : user.id},
-        progress: { progressAt: currentDate, progressByUserId: props.data.userId ? props.data.userId : user.id},
-        review: { reviewAt: currentDate, reviewByUserId: props.data.userId ? props.data.userId : user.id},
-        done: { doneAt: currentDate, doneByUserId: props.data.userId ? props.data.userId : user.id}
+        selected: { selectedAt: currentDate, selectedByUserId: props.data.userId ? props.data.userId : user.id },
+        progress: { progressAt: currentDate, progressByUserId: props.data.userId ? props.data.userId : user.id },
+        review: { reviewAt: currentDate, reviewByUserId: props.data.userId ? props.data.userId : user.id },
+        done: { doneAt: currentDate, doneByUserId: props.data.userId ? props.data.userId : user.id }
     }
 
     const changeTaskStatus = (e: any) => {
-        // e.preventDefault();
-        // debugger;
+
         const checkValue = e.target.innerText;
         if (projectArray.includes(checkValue) || meetingArray.includes(checkValue)) {
-            let url = "http://localhost:62000/api/v1/tasks";
-
-            axios.post(url, {changeData: actionDataObject[checkValue], idData : props.data.taskId ? props.data.taskId : props.data.id, taskProgress:checkValue})
-                .then(result => {
-                    
-                    if(result.status === 200){
-                        window.location.reload();
-                    }
-                })
-                    .catch(err => console.log(err));
+            queryData = {
+                changeData: actionDataObject[checkValue],
+                idData: props.data.taskId ? props.data.taskId : props.data.id,
+                taskProgress: checkValue
+            };
+            getData();
         }
     }
 
-    let dropdownButtonsArray = props.taskType === 'project' ? projectArray.map((element: string, k: number) => {
+    const getData = () => {
+
+        if (Object.keys(queryData).length > 0) {
+            axios.post("http://localhost:62000/api/v1/tasks", queryData)
+                .then(result => {
+
+                    if (result.status === 200) {
+                    }
+                })
+                .catch(err => console.log(err));
+        }
+    }
+
+    let dropdownButtonsArray = props.data.taskType === 'project' ? projectArray.map((element: string, k: number) => {
         return <Dropdown.Item as="button" key={'bbd' + k}>{element}</Dropdown.Item>
-    }) : 
-    meetingArray.map((element: string, k: number) => {
-        return <Dropdown.Item as="button" key={'bbd' + k}>{element}</Dropdown.Item>
-    });
+    }) :
+        meetingArray.map((element: string, k: number) => {
+            return <Dropdown.Item as="button" key={'bbd' + k}>{element}</Dropdown.Item>
+        });
+
+    useEffect(() => {
+        getData();
+    }, [queryData]);
 
     return (
         <Modal
@@ -70,7 +84,7 @@ const MyVerticallyCenteredModal = (props: any) => {
                 </p>
 
                 <Form>
-                    <DropdownButton id="dropdown-item-button" title="Select status" onClick={e => changeTaskStatus(e)}>
+                    <DropdownButton id="dropdown-item-button" title="Select status" onClick={e => { changeTaskStatus(e); getData() }}>
                         {dropdownButtonsArray}
                     </DropdownButton>
                 </Form>
