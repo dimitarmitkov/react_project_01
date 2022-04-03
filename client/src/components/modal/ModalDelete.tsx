7f7b3e719e33712ef3df0c8b21c53989c88900f5
dimitarmitkov <dimitar.mitkov@me.com>
2022-04-03 12:31:01 +0300

    delete user code clean up

--- client/src/components/modal/ModalDelete.tsx
+++ client/src/components/modal/ModalDelete.tsx
@@ -1,73 +1,71 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import ButtonBs from 'react-bootstrap/Button';
 import { Button } from 'primereact/button';
 import axios from 'axios';
 
 const DeleteUserModal = (props: any) => {
-     const [show, setShow] = useState(true);
-  
-    const handleDiscard = () => setShow(false);
-    const handleDelete = () =>{
+  const [show, setShow] = useState(true);
 
-        console.log('delete this user ', props.data.firstName);
+  const getData = () => axios.post("http://localhost:62000/api/v1/usersDelete",
+    {
+      id: props.data.id
+    })
+    .then(res => {
+      if (res.status === 200) {
+        window.location.reload();
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
 
-        axios.post("http://localhost:62000/api/v1/usersDelete",
-        {
-            id: props.data.id
-        })
-        .then(res => {
+  const handleDiscard = () => setShow(false);
+  const handleDelete = () => {
 
+    getData();
 
-            if (res.status === 200) {
-                window.location.reload();
-            }
-        })
-        .catch(err => {
-            console.log(err);
-        });
-
-        setShow(false);
-    } 
-  
-    return (
-      <>
-        <Modal {...props} show={show}>
-          <Modal.Header closeButton>
-            <Modal.Title>Delete User {props.data.firstName}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>You're about to delete this User. Are you sure?</Modal.Body>
-          <Modal.Footer>
-            <ButtonBs variant="secondary" onClick={handleDiscard}>
-              Discard
-            </ButtonBs>
-            <ButtonBs variant="danger" onClick={handleDelete}>
-              Delete
-            </ButtonBs>
-          </Modal.Footer>
-        </Modal>
-      </>
-    );
+    setShow(false);
   }
 
-  const DeleteUserModalApp = (props: any[]) => {
-    const [modalShow, setModalShow] = useState(false);
+  useEffect(()=>{
+    
+  },[])
 
-    return (
-        <>
-            {/* <Button variant="danger" onClick={() => setModalShow(prevCheck => !prevCheck)}> */}
-            <Button className="p-button-warning" onClick={() => setModalShow(prevCheck => !prevCheck)}>
-            {/* <Button variant="danger" onClick={() => setModalShow(true)}> */}
-                Delete User
-            </Button>
+  return (
+    <>
+      <Modal {...props} show={show}>
+        <Modal.Header closeButton onClick={handleDiscard}>
+          <Modal.Title>Delete User {props.data.firstName}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>You're about to delete this User. Are you sure?</Modal.Body>
+        <Modal.Footer>
+          <ButtonBs variant="secondary" onClick={handleDiscard}>
+            Discard
+          </ButtonBs>
+          <ButtonBs variant="danger" onClick={handleDelete}>
+            Delete
+          </ButtonBs>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
 
-            {modalShow ? <DeleteUserModal
-                // onHide={() => setModalShow(false)}
-                data={props}
-            /> : null }
-        </>
-    );
+const DeleteUserModalApp = (props: any[]) => {
+  const [modalShow, setModalShow] = useState(false);
+
+  return (
+    <>
+      <Button className="p-button-warning" onClick={() => setModalShow(prevCheck => !prevCheck)}>
+        Delete User
+      </Button>
+
+      {modalShow ? <DeleteUserModal
+        data={props}
+      /> : null}
+    </>
+  );
 }
 
 export default DeleteUserModalApp;
-  
