6a3304ed5bc5037b6f699d02f27d906a5006b3ba
dimitarmitkov <dimitar.mitkov@me.com>
2022-04-20 16:44:15 +0300

    curren user data code clean up

--- client/src/components/user/currentUserData.ts
+++ client/src/components/user/currentUserData.ts
@@ -1,24 +1,17 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+const CurrentUserCardData = (id: string | undefined) => {
 
-const CurrentUserCardData = (id:string | undefined) => {
-    
     const [currentUserCardData, setCurrentUserCardData] = useState({});
 
     const getUser = () => {
 
         const urlUser = "http://localhost:62000/api/v1/users";
+        const query = { id: id };
+        const headersData = { 'Content-Type': 'application/json' };
 
-        axios.post(urlUser,
-            {
-                id: id
-            },
-            {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
+        axios.post(urlUser, query, { headers: headersData })
             .then(result => {
                 setCurrentUserCardData(result.data);
             })
