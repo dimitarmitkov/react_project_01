06c3a5b6b63a67ccfdeeff21c6063b2b478e5c4f
dimitarmitkov <dimitar.mitkov@me.com>
2022-04-29 17:31:04 +0300

    login, logout error handler set

--- client/src/components/login/LoginForm.tsx
+++ client/src/components/login/LoginForm.tsx
@@ -16,10 +16,15 @@ type FormValues = {
     email: string;
 };
 
+const ErrorComponent = () => {
+    return <h1>Some error appears, please contact sys admin.</h1>
+}
+
 const LoginGroup = () => {
 
     const { register, formState: { errors }, handleSubmit } = useForm<FormValues>();
     const [passwordValue, setPasswordValue] = useState('');
+    const [hasError, setHasError] = useState(false);
 
     const onSubmit: SubmitHandler<FormValues> = data => {
 
@@ -27,53 +32,66 @@ const LoginGroup = () => {
             insertEmail: data.email,
             insertPassword: passwordValue
         };
-
-        axiosFunction('loginForm', query, 'post', 200, undefined, '/helloMitko');
+        try {
+            axiosFunction('loginForm', query, 'post', 200, undefined, '/helloMitko');
+        } catch (error) {
+            setHasError(true);
+        }
     };
+    if (!hasError) {
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
+        return (
+            <form onSubmit={handleSubmit(onSubmit)}>
 
-            <Container>
+                <Container>
 
-                <Row className="mt-3 justify-content-md-center">
-                    <Col sm={3}>
-                        <div className="p-inputgroup">
-                            <span className="p-inputgroup-addon">
-                                <i className="pi pi-envelope"></i>
-                            </span>
-                            <InputText placeholder="Email" {...register("email", {
-                                required: true, pattern: {
-                                    value: /\S+@\S+\.\S+/,
-                                    message: "Entered value does not match email format"
-                                }
-                            })} />
-                        </div>
-                        {errors.email && <span className="error-message" role="alert">{errors.email.message}</span>}
-                    </Col>
-                </Row>
+                    <Row className="mt-3 justify-content-md-center">
+                        <Col sm={3}>
+                            <div className="p-inputgroup">
+                                <span className="p-inputgroup-addon">
+                                    <i className="pi pi-envelope"></i>
+                                </span>
+                                <InputText placeholder="Email" {...register("email", {
+                                    required: true, pattern: {
+                                        value: /\S+@\S+\.\S+/,
+                                        message: "Entered value does not match email format"
+                                    }
+                                })} />
+                            </div>
+                            {errors.email && <span className="error-message" role="alert">{errors.email.message}</span>}
+                        </Col>
+                    </Row>
 
-                <Row className="mt-3 justify-content-md-center">
-                    <Col sm={3}>
-                        <div className="p-inputgroup">
-                            <span className="p-inputgroup-addon">
-                                <i className="pi pi-shield"></i>
-                            </span>
-                            <Password value={passwordValue} onChange={(e) => setPasswordValue(e.target.value)} toggleMask feedback={false} />
-                        </div>
-                        {errors.password && <span className="error-message" role="alert">{errors.password.message}</span>}
-                    </Col>
-                </Row>
+                    <Row className="mt-3 justify-content-md-center">
+                        <Col sm={3}>
+                            <div className="p-inputgroup">
+                                <span className="p-inputgroup-addon">
+                                    <i className="pi pi-shield"></i>
+                                </span>
+                                <Password value={passwordValue} onChange={(e) => {
+                                    try {
+                                        setPasswordValue(e.target.value);
+                                    } catch (error) {
+                                        setHasError(true);
+                                    }
+                                }} toggleMask feedback={false} />
+                            </div>
+                            {errors.password && <span className="error-message" role="alert">{errors.password.message}</span>}
+                        </Col>
+                    </Row>
 
-                <Row className="mt-3 justify-content-md-center">
-                    <Col sm={3}>
-                        <Button label="Submit" className="p-button-danger" disabled={false} />
-                        <div className="mt-2">If you don't have an account please <Link to={`/signup`} className="active-task-link">SingUp</Link></div>
-                    </Col>
-                </Row>
-            </Container>
-        </form>
-    );
+                    <Row className="mt-3 justify-content-md-center">
+                        <Col sm={3}>
+                            <Button label="Submit" className="p-button-danger" disabled={false} />
+                            <div className="mt-2">If you don't have an account please <Link to={`/signup`} className="active-task-link">SingUp</Link></div>
+                        </Col>
+                    </Row>
+                </Container>
+            </form>
+        );
+    } else {
+        return <ErrorComponent />
+    }
 }
 
 export default LoginGroup;
--- client/src/components/login/Logout.tsx
+++ client/src/components/login/Logout.tsx
@@ -4,11 +4,17 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import { Row, Col, Container } from 'react-bootstrap';
 import './loginForm.css';
 import axiosFunction from '../functions/axiosFunctions';
+import { useState } from 'react';
 
 type FormValues = {
 };
 
+const ErrorComponent = () => {
+    return <h1>Some error appears, please contact sys admin.</h1>
+}
+
 const LogoutGroup = () => {
+    const [hasError, setHasError] = useState(false);
 
     const { handleSubmit } = useForm<FormValues>();
 
@@ -16,35 +22,44 @@ const LogoutGroup = () => {
 
         const query = { withCredentials: true };
 
-        axiosFunction('logoutForm', query, 'get', 200, undefined, '/login');
+        try {
+            axiosFunction('logoutForm', query, 'get', 200, undefined, '/login');
+        } catch (error) {
+            setHasError(true);
+        }
     };
+    if (!hasError) {
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            
-            <Container>
+        return (
+            <form onSubmit={handleSubmit(onSubmit)}>
 
-                <Row className="mt-3 justify-content-md-center">
-                    <Col sm={3}>
-                        <h5>You're about to log out</h5>
-                    </Col>
-                </Row>
+                <Container>
 
-                <Row className="mt-3 justify-content-md-center">
-                    <Col sm={3}>
-                        <h6>Please confirm</h6>
-                    </Col>
-                </Row>
+                    <Row className="mt-3 justify-content-md-center">
+                        <Col sm={3}>
+                            <h5>You're about to log out</h5>
+                        </Col>
+                    </Row>
 
-                <Row className="mt-3 justify-content-md-center">
-                    <Col sm={3}>
-                        <label></label>
-                        <Button label="Log Out" className="p-button-danger" disabled={false} />
-                    </Col>
-                </Row>
-            </Container>
-        </form>
-    );
+                    <Row className="mt-3 justify-content-md-center">
+                        <Col sm={3}>
+                            <h6>Please confirm</h6>
+                        </Col>
+                    </Row>
+
+                    <Row className="mt-3 justify-content-md-center">
+                        <Col sm={3}>
+                            <label></label>
+                            <Button label="Log Out" className="p-button-danger" disabled={false} />
+                        </Col>
+                    </Row>
+                </Container>
+            </form>
+        );
+    } else {
+        return <ErrorComponent />
+    }
+
 }
 
 export default LogoutGroup;
