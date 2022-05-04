10761a8d65bfe4c2e5785b8aeb439408052c98c4
dimitarmitkov <dimitar.mitkov@me.com>
2022-05-04 17:24:16 +0300

    add link for current logged user in enumerator.ts

--- client/src/components/functions/currentLoggedUser.ts
+++ client/src/components/functions/currentLoggedUser.ts
@@ -1,20 +1,22 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { valuesLinks } from '../../enumerators';
 
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 const CurrentLoggedUser = () => {
 
     const [user, setUser] = useState();
-    
-    const url = SERVER_URL+"/currentLoggedUser";
-    
+
+    const url = SERVER_URL + valuesLinks.CurrentLoggedUser;
+
     function axiosFunction() {
 
-        axios.get(url, {withCredentials: true})
+        axios.get(url, { withCredentials: true })
             .then(response => {
 
-                setUser(response.data)})
+                setUser(response.data)
+            })
             .catch(err => {
                 console.log('No user logged');
             });
@@ -28,4 +30,3 @@ const CurrentLoggedUser = () => {
 }
 
 export default CurrentLoggedUser;
-    
--- client/src/enumerators.ts
+++ client/src/enumerators.ts
@@ -9,23 +9,25 @@ export const valuesMeetingProgress = ["selected", "progress", "done"];
 export const valuesTaskType = { Project: "project", Meeting: "meeting" };
 
 export const valuesLinks = {
-    TasksPage: "/tasksPage",
-    UsersPage: "/usersPage",
     CreateTask: "/createTask",
-    Tasks: "/tasks",
-    Users: "/users",
-    UserTasks: "/usertasks",
-    UsersTasksMop: "/usertasksmop",
-    SignUp: "/signup",
+    CurrentLoggedUser: "/currentLoggedUser",
     DashBoard:"/helloMitko",
     LogIn: "/login",
     PhotosUpload: "/photos/upload",
-    UsersEdit: "/usersEdit",
-    UsersDelete: "/usersDelete",
+    SignUp: "/signup",
+    Tasks: "/tasks",
     TasksDelete: "/tasksDelete",
+    TasksPage: "/tasksPage",
+    Users: "/users",
+    UserCreate: "/createUser",
     UserLogin: "/userLogin",
     UserLogout: "/userLogout",
-    UserCreate: "/createUser"
+    UserTasks: "/usertasks",
+    UsersDelete: "/usersDelete",
+    UsersEdit: "/usersEdit",
+    UsersPage: "/usersPage",
+    UsersTasksMop: "/usertasksmop"
+
 };
 
 export const valuesUsersTypes = { Admin: "admin", User: "user" };
